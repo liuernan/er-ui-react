e432198ee4582d7d3269496ca8c481d41888871c
Jiawei <liujiaweidreamer@gmail.com>
2021-08-29 19:04:02 +0800

    yarn add webpack-dev-server --dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  // mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
   entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js", "jsx"]
+    extensions: ['.ts', '.tsx', '.js', 'jsx']
   },
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      {test: /\.tsx?$/, loader: 'ts-loader'}
     ]
   }
 };
