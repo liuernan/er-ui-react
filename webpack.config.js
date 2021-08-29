8fecfe55182383e1871076e1006599f454b4b771
Jiawei <liujiaweidreamer@gmail.com>
2021-08-29 18:52:38 +0800

    add config for bundling .tsx? file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  // mode: "development",
+  devtool: "inline-source-map",
+  entry: './src/index.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", "jsx"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
 };
