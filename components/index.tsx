ef297818eb02b7b2fc27bb1679e5c7594f5b8680
Jiawei <liujiaweidreamer@gmail.com>
2021-09-07 16:48:16 +0800

    create components directory

---
+++ components/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      hi
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
