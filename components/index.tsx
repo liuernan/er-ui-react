0d69570547fc401f12cf6d118fa1517d57fd8798
Jiawei <liujiaweidreamer@gmail.com>
2021-09-07 16:56:40 +0800

    create Icon component, svg icons can be show with props `name`

--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Icon from './icon/Icon';
 
 const App = () => {
   return (
     <div>
-      hi
+      <Icon name='heart'/>
+      <Icon name='github-fill'/>
+      <Icon name='up'/>
+      <Icon name='down'/>
+      <Icon name='left'/>
+      <Icon name='right'/>
+      <Icon name='zhihu'/>
+      <Icon name='wechat-fill'/>
+      <Icon name='chrome'/>
+      <Icon name='IE'/>
     </div>
   );
 };
