afd816079a2969be665745157a4ebf3c9376a88c
Jiawei <liujiaweidreamer@gmail.com>
2021-09-07 15:41:43 +0800

    yarn add svg-sprite-loader -D

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      {test: /\.tsx?$/, loader: 'ts-loader'}
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {  // https://github.com/JetBrains/svg-sprite-loader
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader'
+      }
     ]
   }
 };
