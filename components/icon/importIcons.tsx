c901b0912902e726bd7e3d9c82120016163948e3
Jiawei <liujiaweidreamer@gmail.com>
2021-09-08 15:37:09 +0800

    fix ts error

--- components/icon/importIcons.tsx
+++ components/icon/importIcons.tsx
@@ -1,7 +1,7 @@
-let importAll = (requireContext:any) => requireContext.keys().forEach(requireContext)
+const importAllSVG = (requireContext:any) => requireContext.keys().forEach(requireContext)
 try {
   // @ts-ignore
-  importAll(require.context('./svg/', true, /\.svg$/))
+  importAllSVG(require.context('./svg/', true, /\.svg$/))
 } catch (error) {
   console.log(error)
 }
